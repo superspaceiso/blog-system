f639adb2f1645ef925bf712fcb908d03240e4577
Scott Cottam <scott@scottcottam.co.uk>
2018-03-09 02:01:39 +0000

    Updated Posting Script
    Now includes comma seperated tags and checking to see if tags already
    exist within the database.

--- app/postblog.php
+++ app/postblog.php
@@ -7,18 +7,23 @@ $user_id =$_POST["id"];
 $blog_title=$_POST["title"];
 $blog_content=$_POST["content"];
 $blog_date = date("Y-m-d");
+$tags = explode(",",$_POST["tags"]);
 
-$sql = 'INSERT INTO blog_posts (userid, blogtitle, blogcontent, blogdate) VALUES (?,?,?,?)';
-$stmt= $pdo->prepare($sql);
-$stmt->execute([$user_id, $blog_title, $blog_content, $blog_date]);
+$postcontent = $pdo->prepare('INSERT INTO blog_posts (userid, blogtitle, blogcontent, blogdate) VALUES (?,?,?,?)');
+$posttags = $pdo->prepare('INSERT INTO test_tags (tag) VALUES (?)');
+$tagsearch = $pdo->prepare('SELECT tag FROM test_tags WHERE tag=?');
 
-if ($stmt->execute() == 1 ) {
-  echo "Success";
-  //header("location: ../controlpanel.php");
-} else {
-  echo "Fail";
+foreach ($tags as $blog_tag) {
+  $tagsearch->execute([$blog_tag]);
+  $foundtags = $tagsearch->rowCount();
+  if ($foundtags == 0) {
+    $posttags->execute([$blog_tag]);
+    $postcontent->execute([$user_id, $blog_title, $blog_content, $blog_date]);
+    header("location: ../controlpanel.php");
+  } else {
+    $postcontent->execute([$user_id, $blog_title, $blog_content, $blog_date]);
+    header("location: ../controlpanel.php");
+  }
 }
-
-
 
 ?>
--- controlpanel.php
+++ controlpanel.php
@@ -12,15 +12,26 @@ header('Location: login.php');
 
 <a href="./app/logoutscript.php">Log Out</a>
 
+<div class="row">
+  <div class="tweleve columns">
+
 <h1>New Post</h1>
 <form role="form" action="./app/postblog.php" method="post">
   <input type="hidden" name="id" value="<?php echo $_SESSION["id"]; ?>">
-  <input type="text" name="title" rows="8" cols="80"></input><br>
-  <textarea name="content" rows="8" cols="80"></textarea><br>
-  <button type="submit" name="submit">Post</button>
+  <input class="u-full-width" type="text" name="title" placeholder="Title"></input><br>
+  <textarea class="u-full-width" name="content" placeholder="Content"></textarea><br>
+  <input  class="u-full-width" type="text" name="tags" placeholder="Tags, seperate using a comma eg Eggs,Milk,Flour"></input><br>
+  <button class="button-primary" type="submit" name="submit">Post</button>
 </form>
 
+  </div>
+</div>
+<div class="row">
+<div class="tweleve columns">
 <h1>Previous Posts</h1>
 <?php require './app/bloglist.php'; ?>
+
+</div>
+</div>
 
 <?php require_once "./includes/footer.php" ?>
